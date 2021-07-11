e71a76a88222a32b313ac2feaea26f05bba3dc45
2420gyy <2420191325@qq.com>
2021-07-11 23:48:44 +0800

    gitTest01修改

--- src/test/java/GitTest.java
+++ src/test/java/GitTest.java
@@ -4,6 +4,8 @@ public class GitTest {
     @Test
     public void test1(){
         System.out.println("gittest");
+        System.out.println("gittest1");
+        System.out.println("gittest2");
     }
 
 }
