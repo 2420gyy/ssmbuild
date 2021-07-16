e5cdef6fc600e26959c4b9fb038a371748866a59
2420gyy <2420191325@qq.com>
2021-07-16 14:43:34 +0800

    dev和master更新操作

--- src/test/java/BranchTest.java
+++ src/test/java/BranchTest.java
@@ -1,6 +1,7 @@
 public class BranchTest {
     public static void main(String[] args) {
         System.out.println("新的分支");
+        System.out.println("master->test01");
         System.out.println("dev->test01");
     }
 }
